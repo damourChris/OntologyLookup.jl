9bca04b74af2f1b70efcbaa48a87aa3db70729f9
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-06-16 23:51:25 +0000

    feat: update OntologyLookup module with OLS_BASE_URL constant and new modules loading

--- src/OntologyLookup.jl
+++ src/OntologyLookup.jl
@@ -1,5 +1,17 @@
 module OntologyLookup
 
-# Write your package code here.
+const OLS_BASE_URL = "https://www.ebi.ac.uk/ols4/api/"
+export OLS_BASE_URL
+
+include("terms.jl")
+include("client.jl")
+using .OLSClient
+
+include("search.jl")
+using .Search
+export search
+
+function __init__()
+end
 
 end
