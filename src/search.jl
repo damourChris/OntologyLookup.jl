fcdfbaa5c7732048fabfde60ce7e8e8f2ad474fc
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-06-17 10:18:01 +0000

    doc: add doc strings for search and OLSquery

--- src/search.jl
+++ src/search.jl
@@ -8,6 +8,18 @@ import ..OntologyLookup: OLS_BASE_URL, Term
 
 export search
 
+"""
+    OLSquery(q::Dict)
+
+Query the Ontology Lookup Service (OLS) with the given parameters.
+
+# Arguments
+- `q::Dict`: A dictionary containing the query parameters.
+
+# Returns
+- `data`: The response data from the OLS search.
+
+"""
 function OLSquery(q::Dict)
     url = OLS_BASE_URL * "search"
     response = OLSClient.get(url; query=q)
@@ -19,6 +31,20 @@ function OLSquery(q::Dict)
     return data
 end
 
+"""
+    search(q::String; [rows::Int=10, ontology::String=""])
+
+Searches for ontology terms from the Ontology Lookup Service (OLS) using a search query.
+
+# Arguments
+- `q::String`: The search query.
+- `rows::Int`: The maximum number of rows to return.
+- `ontology::String`: The ontology to search in.
+
+# Returns
+A DataFrame containing the search results.
+
+"""
 function search(q::String; rows::Int=10, ontology::String="")
     q = Dict("q" => q, "rows" => rows, "ontology" => ontology)
     data = OLSquery(q)
