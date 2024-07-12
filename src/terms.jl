85585a1658400aebf5384bb6d7b4264f1f2a0cde
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-07-12 16:45:45 +0200

    feat: add extra optional fields to Term struct update constructor

--- src/terms.jl
+++ src/terms.jl
@@ -1,17 +1,28 @@
 struct Term
     iri::String
-    label::String
     ontology_name::String
     ontology_prefix::String
     short_form::String
-    description::Array{String}
     obo_id::String
+    description::Array{String}
+    lang::Union{String,Missing}
+    label::Union{String,Missing}
+    ontology_iri::Union{String,Missing}
+    synonyms::Union{Array{String},Missing}
+    is_preferred_root::Union{Bool,Missing}
 end
 
 function Term(d::AbstractDict)
-    @assert haskey(d, "iri") && haskey(d, "label") && haskey(d, "ontology_name") &&
-            haskey(d, "ontology_prefix") && haskey(d, "short_form") &&
-            haskey(d, "description") && haskey(d, "obo_id")
-    return Term(d["iri"], d["label"], d["ontology_name"], d["ontology_prefix"],
-                d["short_form"], d["description"], d["obo_id"])
+    @assert haskey(d, "iri") && haskey(d, "ontology_name") &&
+            haskey(d, "ontology_prefix") &&
+            haskey(d, "short_form") && haskey(d, "obo_id") && haskey(d, "description") "Unable to construct Term from dictionary."
+    lang = get(d, "lang", missing)
+    label = get(d, "label", missing)
+    ontology_iri = get(d, "ontology_iri", missing)
+    synonyms = get(d, "synonyms", missing)
+    is_preferred_root = get(d, "is_preferred_root", missing)
+
+    return Term(d["iri"], d["ontology_name"], d["ontology_prefix"], d["short_form"],
+                d["obo_id"], d["description"], lang, label, ontology_iri, synonyms,
+                is_preferred_root)
 end
