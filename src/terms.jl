7b3330bff467c3c7fa11575c1c312f3cd45233b7
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-07-16 14:10:18 +0000

    feat: add custom display for Term tructs

--- src/terms.jl
+++ src/terms.jl
@@ -26,3 +26,34 @@ function Term(d::AbstractDict)
                 d["obo_id"], d["description"], lang, label, ontology_iri, synonyms,
                 is_preferred_root)
 end
+
+function Base.show(io::IO, term::Term)
+    println(io, "Term:")
+    println(io, "  IRI: ", term.iri)
+    println(io, "  Ontology Name: ", term.ontology_name)
+    println(io, "  Ontology Prefix: ", term.ontology_prefix)
+    println(io, "  Short Form: ", term.short_form)
+    println(io, "  OBO ID: ", term.obo_id)
+
+    # Handling array fields
+    println(io, "  Description(s): ", join(term.description, ", "))
+
+    # Handling optional fields
+    print_optional(io, "Language", term.lang)
+    print_optional(io, "Label", term.label)
+    print_optional(io, "Ontology IRI", term.ontology_iri)
+
+    # Special handling for synonyms array
+    if !ismissing(term.synonyms)
+        println(io, "  Synonyms: ", join(term.synonyms, ", "))
+    end
+
+    return print_optional(io, "Is Preferred Root", term.is_preferred_root)
+end
+
+# Helper function to handle optional fields
+function print_optional(io::IO, name::String, value::Union{T,Missing}) where {T}
+    if !ismissing(value)
+        println(io, "  $name: ", value)
+    end
+end
