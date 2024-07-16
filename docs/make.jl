276ac86f22fb5b06a89e9790c0ff8fb3dda17855
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-07-16 15:16:23 +0000

    ci: add doc preview on pull request
    also remove devbranch arg as unsure what it does
    
    note the gh workflow was based on CliMA/ClimaTimeSteppers.jl (Apache License 2.0).

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,18 @@ using Documenter
 DocMeta.setdocmeta!(OntologyLookup, :DocTestSetup, :(using OntologyLookup); recursive=true)
 
 makedocs(;
-    modules=[OntologyLookup],
-    authors="Chris Damour",
-    sitename="OntologyLookup.jl",
-    format=Documenter.HTML(;
-        canonical="https://damourChris.github.io/OntologyLookup.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Search" => "search.md",
-        "Term Controller" => "term_controller.md",
-        "Reference" => "reference.md"
-    ],
-)
+         modules=[OntologyLookup],
+         authors="Chris Damour",
+         sitename="OntologyLookup.jl",
+         format=Documenter.HTML(;
+                                canonical="https://damourChris.github.io/OntologyLookup.jl",
+                                edit_link="main",
+                                assets=String[],),
+         pages=["Home" => "index.md",
+                "Search" => "search.md",
+                "Term Controller" => "term_controller.md",
+                "Reference" => "reference.md"],)
 
 deploydocs(;
-    repo="github.com/damourChris/OntologyLookup.jl",
-    devbranch="main",
-)
+           repo="github.com/damourChris/OntologyLookup.jl.git",
+           push_preview=true,)
